d2d13bb371bcf87215d2b6ce1065d281b48f78c0
Blaxav <xav.blanchot@gmail.com>
2024-07-02 00:04:20 +0200

    read all commands and copy them into dirs

--- claire/command.py
+++ claire/command.py
@@ -4,50 +4,33 @@ from pathlib import Path
 def help():
     """Help function."""
     print()
-    print("Usage: python main.py --data <data_path> --target <target_path>")
-    print(
-        "\nOptional: --target <target_path> to speicfy where to write the final photos.\n"
-    )
+    print("Usage: python main.py --data <data_path> ")
     exit()
 
 
-def read_args(args: list[str]) -> tuple[Path, Path, Path]:
+def read_args(args: list[str]) -> Path:
     """
     Arguments:
-    --command: Path to the excel of the command
     --data : Directory to find photos
-    --target : Directory to save order, if no target provided, data will be written
-        where the command is launched
-    Returns a 3-tuple of Path objects, the data_path, the target dir and the command_file.
+    Returns the data_path.
     """
     data_path: Path | None = None
-    command_file: Path | None = None
 
     target: str = Path("./commands/")
-
     n_args = len(args)
     index = 1
     while index < n_args:
         if args[index] == "--data":
-            data = Path(args[index + 1])
-            index += 2
-        elif args[index] == "--target":
-            target = Path(args[index + 1])
-            index += 2
-        elif args[index] == "--command":
-            command_file = Path(args[index + 1])
-            if command_file.suffix != ".xlsx":
-                print("Command file must be an excel file.")
-                help()
+            data_path = Path(args[index + 1])
             index += 2
         elif args[index] == "--help":
             help()
         else:
             index += 1
 
-    if data_path is None or command_file is None:
+    if data_path is None:
         print("Invalid arguments.")
         print("Use --help for more information.")
         help()
 
-    return data_path, target, command_file
+    return data_path
--- claire/main.py
+++ claire/main.py
@@ -1,32 +1,80 @@
 import shutil
-from .command import read_args
+from command import read_args
 from sys import argv
 from pathlib import Path
+import pandas as pd
+import numpy as np
+import os
+
+
+PHOTO_EXTENSION = ".JPG"
 
 
 def read_order_excel(command_file: Path) -> list[str]:
     """Read the excel for the current order and returns the list of required files."""
-    raise NotImplementedError()
+    data = pd.read_excel(command_file)
+    # Fill missing data with python's None
+    data = data.fillna(np.nan).replace([np.nan], [None])
+    res = []
 
-
-def copy_files(
-    data_path: Path, target: Path, files: list[Path], subdir_name: str | None = None
-):
-    """Copy files from data_path to target."""
-    if subdir_name is not None:
-        final_dir = target / subdir_name
-        final_dir.mkdir(parents=True, exist_ok=True)
-
-    for file in files:
+    row_id = 0
+    col_id = 0
+    found = False
+    while not found:
         try:
-            shutil.copy(data_path / file, target)
+            value = data.iloc[row_id, col_id]
+            if value == "N° Photo":
+                found = True
+                break
+            else:
+                col_id += 1
+        except IndexError:
+            row_id += 1
+            col_id = 0
+
+    row_id += 1
+    while True:
+        val = data.iloc[row_id, col_id]
+        if val is not None:
+            res.append(str(val))
+            row_id += 1
+        else:
+            break
+    return res
+
+
+def copy_files(data_path: Path, target: Path, files: list[Path]):
+    """Copy files from data_path to target."""
+
+    target.mkdir(parents=True, exist_ok=True)
+
+    for order in orders:
+        filename = order + PHOTO_EXTENSION
+        try:
+            shutil.copy(data_path / filename, target / filename)
         except FileNotFoundError:
-            print(f"Cannot find {data_path / file}.")
+            print(f"Cannot find {data_path / filename}.")
             print("Please check the path of data.")
 
 
 if __name__ == "__main__":
-    data_path, target_dir, command_file = read_args(argv)
+    data_path, _, _ = read_args(argv)
 
-    orders = read_order_excel(command_file)
-    copy_files(data_path, target_dir, orders)
+    # Get files prefix
+    prefixes = set()
+    for _file in os.listdir(data_path):
+        if _file.endswith(PHOTO_EXTENSION):
+            prefixes.add(_file[:4])
+
+    if len(prefixes) > 1:
+        raise ValueError("All files must have the same four charactersprefix.")
+
+    prefix = list(prefixes)[0]
+
+    for _file in os.listdir(data_path):
+        if _file.endswith(".xlsx"):
+            command_file = data_path / _file
+            orders = [prefix + elt for elt in read_order_excel(command_file)]
+
+            target = data_path / _file.split(".", 1)[0].replace(" ", "_")
+            copy_files(data_path, target, orders)
